f41fc658dce98cda39d8987044eccc4ae589cf0b
chenming <852098505@qq.com>
2023-07-11 14:20:12 +0800

    update yxip

--- yxip.sh
+++ yxip.sh
@@ -19,7 +19,7 @@ tail -n +2 result.csv | while IFS=',' read -r col1 col2 col3 col4 col5 col6; do
     fi
   echo "优选ip-$counter: $col1 , 速度: $col6"
   echo "vless://e220a9aa-5d2d-4db4-9e14-4968e52b31ca@$col1:443?encryption=none&security=tls&sni=vless.cmhub.tk&fp=randomized&type=ws&host=vless.cmhub.tk&path=%2F%3Fed%3D2048#$ISP-oneself-$counter【${col5}ms | $col6】" >> "$ISP.list"
-  ((counter++))
+  counter=$((counter+1))
   if [ $counter -ge 6 ]; then
     echo "数量够5个，跳出循环"
     break
